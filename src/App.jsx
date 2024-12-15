ed5b8973cacb40b04a57a41ee4b20872990f05e5
GianBo <162351219+Fonchosama@users.noreply.github.com>
2024-12-15 18:12:02 +0100

    Exercise complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+import EventList from './components/EventList';
+import CitySearch from './components/CitySearch';
+
+const App = () => {
+  return (
+    <div>
+      <CitySearch />
+      <EventList />
+    </div>
+  );
+};
+
+export default App;
