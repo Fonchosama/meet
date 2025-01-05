1d39f8ab577f4934f3d0278b87636fdc52d03d4d
GianBo <162351219+Fonchosama@users.noreply.github.com>
2025-01-05 17:03:11 +0100

    Feature 1: Filter Events by City (Scenario 1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-// src/App.jsx
+// src/App.js
 import React from 'react';
+import { useEffect, useState } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import { getEvents } from './api';
+import './App.css';
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  };
+
   return (
     <div>
       <CitySearch />
-      <EventList />
       <NumberOfEvents />
+      <EventList events={events} />
     </div>
   );
 };
