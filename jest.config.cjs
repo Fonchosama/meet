514ccb10265f9eadd69f4eb0bd897f31c99f9552
GianBo <162351219+Fonchosama@users.noreply.github.com>
2025-01-06 11:15:07 +0100

    coverage jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -8,4 +8,14 @@ module.exports = {
   },
   moduleFileExtensions: ['js', 'jsx'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'], // Path to the setup file
+
+  // Habilita cobertura de código
+  collectCoverage: true, // Activa la recolección de cobertura
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}', // Especifica los archivos a incluir
+    '!src/**/*.test.{js,jsx}', // Excluye archivos de prueba
+    '!src/**/index.{js,jsx}', // Opcional: excluye archivos de punto de entrada
+  ],
+  coverageDirectory: '<rootDir>/coverage', // Directorio donde guardar los informes
+  coverageReporters: ['text', 'lcov'], // Define los formatos del informe
 };
