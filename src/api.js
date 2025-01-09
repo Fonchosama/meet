2007f84d225a48a7be574103656b0967f56e2bc8
GianBo <162351219+Fonchosama@users.noreply.github.com>
2025-01-09 09:04:34 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -50,74 +50,74 @@ export const getEvents = async () => {
     return mockData;
   }
 
-//   const token = await getAccessToken();
+  //   const token = await getAccessToken();
 
-//   if (token) {
-//     removeQuery();
-//     const url =
-//       'https://2ec5nwywrk.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' +
-//       '/' +
-//       token;
-//     const response = await fetch(url);
-//     const result = await response.json();
-//     if (result) {
-//       return result.events;
-//     } else return null;
-//   }
-// };
+  //   if (token) {
+  //     removeQuery();
+  //     const url =
+  //       'https://2ec5nwywrk.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' +
+  //       '/' +
+  //       token;
+  //     const response = await fetch(url);
+  //     const result = await response.json();
+  //     if (result) {
+  //       return result.events;
+  //     } else return null;
+  //   }
+  // };
 
-// const getToken = async (code) => {
-//   const encodeCode = encodeURIComponent(code);
-//   const response = await fetch(
-//     'https://2ec5nwywrk.execute-api.eu-central-1.amazonaws.com/dev/api/token' +
-//       '/' +
-//       encodeCode
-//   );
-//   const { access_token } = await response.json();
-//   access_token && localStorage.setItem('access_token', access_token);
+  // const getToken = async (code) => {
+  //   const encodeCode = encodeURIComponent(code);
+  //   const response = await fetch(
+  //     'https://2ec5nwywrk.execute-api.eu-central-1.amazonaws.com/dev/api/token' +
+  //       '/' +
+  //       encodeCode
+  //   );
+  //   const { access_token } = await response.json();
+  //   access_token && localStorage.setItem('access_token', access_token);
 
-//   // This function takes the code and encode it using the encodeURIComponent
+  //   // This function takes the code and encode it using the encodeURIComponent
 
-//   return access_token;
-// };
+  //   return access_token;
+  // };
 
-// const getToken = async (code) => {
-//   try {
-//     const encodeCode = encodeURIComponent(code);
+  // const getToken = async (code) => {
+  //   try {
+  //     const encodeCode = encodeURIComponent(code);
 
-//     const response = await fetch(
-//       'https://2ec5nwywrk.execute-api.eu-central-1.amazonaws.com/dev/api/token' +
-//         '/' +
-//         encodeCode
-//     );
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! status: ${response.status}`);
-//     }
-//     const { access_token } = await response.json();
-//     access_token && localStorage.setItem('access_token', access_token);
-//     return access_token;
-//   } catch (error) {
-//     error.json();
-//   }
-// };
+  //     const response = await fetch(
+  //       'https://2ec5nwywrk.execute-api.eu-central-1.amazonaws.com/dev/api/token' +
+  //         '/' +
+  //         encodeCode
+  //     );
+  //     if (!response.ok) {
+  //       throw new Error(`HTTP error! status: ${response.status}`);
+  //     }
+  //     const { access_token } = await response.json();
+  //     access_token && localStorage.setItem('access_token', access_token);
+  //     return access_token;
+  //   } catch (error) {
+  //     error.json();
+  //   }
+  // };
 
-// export const getAccessToken = async () => {
-//   const accessToken = localStorage.getItem('access_token');
-//   const tokenCheck = accessToken && (await checkToken(accessToken));
+  // export const getAccessToken = async () => {
+  //   const accessToken = localStorage.getItem('access_token');
+  //   const tokenCheck = accessToken && (await checkToken(accessToken));
 
-//   if (!accessToken || tokenCheck.error) {
-//     await localStorage.removeItem('access_token');
-//     const searchParams = new URLSearchParams(window.location.search);
-//     const code = await searchParams.get('code');
-//     if (!code) {
-//       const response = await fetch(
-//         'https://2ec5nwywrk.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url'
-//       );
-//       const result = await response.json();
-//       const { authUrl } = result;
-//       return (window.location.href = authUrl);
-//     }
-//     return code && getToken(code);
-//   }
-//   return accessToken;
-// };
+  //   if (!accessToken || tokenCheck.error) {
+  //     await localStorage.removeItem('access_token');
+  //     const searchParams = new URLSearchParams(window.location.search);
+  //     const code = await searchParams.get('code');
+  //     if (!code) {
+  //       const response = await fetch(
+  //         'https://2ec5nwywrk.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url'
+  //       );
+  //       const result = await response.json();
+  //       const { authUrl } = result;
+  //       return (window.location.href = authUrl);
+  //     }
+  //     return code && getToken(code);
+  //   }
+  //   return accessToken;
+};
