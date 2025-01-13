ca354b4a43bac37283f4c3d6e11a8ba2b83c7886
GianBo <162351219+Fonchosama@users.noreply.github.com>
2025-01-13 18:46:26 +0100

    Update filterEventsByCity.test.js

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,4 +1,8 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
+import React from 'react';
+import { loadFeature, defineFeature } from 'jest-cucumber';
+import { render } from '@testing-library/react';
+import App from '../App';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
