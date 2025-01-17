27840b3add692f8658ff1bdff2b459cfa8d717d9
GianBo <162351219+Fonchosama@users.noreply.github.com>
2025-01-17 22:52:20 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import * as atatus from 'atatus-spa';
+atatus.config('ccb81aace89e4d5fbf32ed7310201fdc').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+atatus.notify(new Error('Test Atatus Setup'));
