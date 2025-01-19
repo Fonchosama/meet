5448fbb62bb09eb5021978acd65cb667091ce654
GianBo <162351219+Fonchosama@users.noreply.github.com>
2025-01-19 18:08:09 +0100

    Update Alert.jsx

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -42,4 +42,12 @@ class InfoAlert extends Alert {
   }
 }
 
+class ErrorAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'rgb(255, 0, 0)'; // red
+    this.bgColor = 'rgb(242, 197, 199)'; // light red
+  }
+}
+
 export { InfoAlert };
