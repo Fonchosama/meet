c965fa66db24b52a097a2e48d90b1ec3d9fd5049
GianBo <162351219+Fonchosama@users.noreply.github.com>
2025-01-21 18:55:24 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -49,18 +49,23 @@ export const getEvents = async () => {
   if (window.location.href.startsWith('http://localhost')) {
     return mockData;
   }
-
+  if (!navigator.onLine) {
+    const events = localStorage.getItem('lastEvents');
+    NProgress.done();
+    return events ? JSON.parse(events) : [];
+  }
   const token = await getAccessToken();
 
   if (token) {
     removeQuery();
     const url =
-      'https://2ec5nwywrk.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' +
-      '/' +
+      'https://u7ki2qrqc3.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/' +
       token;
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem('lastEvents', JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
