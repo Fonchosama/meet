dd14072b29b54a6d5277caf89e05825da8358a3c
GianBo <162351219+Fonchosama@users.noreply.github.com>
2025-01-21 17:16:27 +0100

    servoce worker & registration created

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 atatus.config('ccb81aace89e4d5fbf32ed7310201fdc').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,3 +11,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+serviceWorkerRegistration.register();
