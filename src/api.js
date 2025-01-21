f431f815b87d43f2bd410a82f3b9824653717ebd
GianBo <162351219+Fonchosama@users.noreply.github.com>
2025-01-21 21:05:50 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 // src/api.js
 
 import mockData from './mock-data';
+import NProgress from 'nprogress';
 
 /**
  *
@@ -61,6 +62,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem('lastEvents', JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
