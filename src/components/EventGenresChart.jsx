bc005031d16f3356da362e32a4dd8c09b86334bd
GianBo <162351219+Fonchosama@users.noreply.github.com>
2025-01-25 19:22:45 +0100

    Update EventGenresChart.jsx

--- src/components/EventGenresChart.jsx
+++ src/components/EventGenresChart.jsx
@@ -1,14 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import {
-  ScatterChart,
-  Scatter,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
   ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell,
+  Legend,
+  Tooltip,
 } from 'recharts';
 
-const EventGenresChart = ({ events }) => {};
+const EventGenresChart = ({ events }) => {
+  const data = [
+    { name: 'Rock', value: 400 },
+    { name: 'Pop', value: 300 },
+    { name: 'Jazz', value: 300 },
+    { name: 'Hip-Hop', value: 200 },
+  ];
 
-export default EventsGenresChart;
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+  return (
+    <ResponsiveContainer width="99%" height={400}>
+      <PieChart width={400} height={400}>
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          outerRadius={150}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default EventGenresChart;
