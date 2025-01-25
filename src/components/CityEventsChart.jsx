36abf70343d243593f67fa79920d36543da43e1e
GianBo <162351219+Fonchosama@users.noreply.github.com>
2025-01-25 12:35:40 +0100

    Update CityEventsChart.jsx

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -23,7 +23,7 @@ const CityEventsChart = ({ allLocations, events }) => {
       const count = events.filter(
         (event) => event.location === location
       ).length;
-      const city = location.split(', ')[0];
+      const city = location.split(/, | - /)[0];
       return { city, count };
     });
     return data;
