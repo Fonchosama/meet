af773af6d14f5478adc89a8b59345a5d1888bd72
GianBo <162351219+Fonchosama@users.noreply.github.com>
2025-01-25 11:54:02 +0100

    Update CityEventsChart.jsx

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -39,8 +39,13 @@ const CityEventsChart = ({ allLocations, events }) => {
         }}
       >
         <CartesianGrid />
-        <XAxis type="number" dataKey="x" name="stature" unit="cm" />
-        <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+        <XAxis type="category" dataKey="city" name="City" />
+        <YAxis
+          type="number"
+          dataKey="countnumber"
+          name="Number of events"
+          allowDecimals={false}
+        />
         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Scatter name="A school" data={data} fill="#8884d8" />
       </ScatterChart>
